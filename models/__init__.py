6addd142f3559370b63ba1440261af3c70093167
Jerry Ding <13386637338@qq.com>
2024-08-08 22:58:19 +0800

    config fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) 2024. All rights reserved.
-from .submodule import model_loss, calc_IoU, model_iou, eval_metric, scene_epe, foreground_epe
+from .submodule import model_loss, calc_IoU, model_iou, eval_metric
 from .odtformer import ref_points_generator, ODTFormer
 from . import wrappers
